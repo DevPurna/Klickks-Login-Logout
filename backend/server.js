8a586aca5a5c962e3bcfb13061724f230fa3ea11
Purna <purnapsch@gmail.com>
2025-09-08 16:50:56 +0530

    backend>>server.js code updated0.4

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // frontend URL
+    origin: "https://klickks-frontend-ten.vercel.app", // frontend URL
     credentials: true,
   })
 );
