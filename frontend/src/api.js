b5b7c83ed866de3e77fce31d15e998937a4a5b64
Purna <purnapsch@gmail.com>
2025-09-08 16:30:23 +0530

    frontend >>api.js code updated0.3

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 // src/api.js
 import axios from "axios";
 
+const API_BASE =
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api/auth";
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: API_BASE,
   withCredentials: true,
 });
